7b70c84a042b35b593780e7ee02884725eee8e83
Houssein Abil <milameet@gmail.com>
2018-08-28 09:15:18 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -2,6 +2,9 @@ def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 
-puts "Hello programmer what is your name"
-name = gets.chomp
-greeting(name)
+greeting("Houssein")
+
+puts "Whats is your dream"
+answer = gets.chomp
+
+puts "Work hard you gonna become a great #{answer}"
